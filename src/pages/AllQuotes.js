6e8b2b8cf63cb7076b0b90b2f0890f26a7eec741
Deepak Kumar <dkiitr15@gmail.com>
2022-08-03 15:41:27 +0530

    first commit

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,68 @@
+import { useEffect } from 'react';
+
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(
+    getAllQuotes,
+    true
+  );
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+};
+
+export default AllQuotes;
+
+
+
+
+// import { Fragment } from "react";
+// import QuoteList from "../components/quotes/QuoteList";
+
+// const DUMMY_QUOTES = [
+//     {
+//         id: "q1",
+//         author: "Max",
+//         text: "Learning React is fun !",
+//     },
+//     {
+//         id: "q2",
+//         author: "Peter",
+//         text: "Leraning React is Great !",
+//     },
+// ];
+
+// const AllQuotes = () => {
+//     return (
+//         <Fragment>
+            
+//             <QuoteList quotes={DUMMY_QUOTES} />
+//         </Fragment>
+//     );
+// };
+
+// export default AllQuotes;
